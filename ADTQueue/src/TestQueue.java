572dff3f2ed2e29be5e66694a51e4a6251a0e749
prashathc <prashathc@gmail.com>
2020-12-19 21:20:44 -0500

    fixed test class

--- ADTQueue/src/TestQueue.java
+++ ADTQueue/src/TestQueue.java
@@ -6,16 +6,16 @@ public class TestQueue {
 
         queue.enqueue("Kyle Lowry");
 
-        System.out.println(queue.toString());
+        System.out.println("The queue: " + queue.toString());
 
-        queue.enqueue("Fred VanVleet");
         queue.enqueue("Pascal Siakam");
+        queue.enqueue("Fred VanVleet");
 
-        System.out.println(queue.toString());
+        System.out.println("The queue: " + queue.toString());
 
-        System.out.println(queue.peek());
+        System.out.println("The front of the queue is: " + queue.peek());
         queue.dequeue();
-        System.out.println(queue.peek());
+        System.out.println("The front of the queue is: " + queue.peek());
 
         queue.dequeueAll();
         System.out.println(queue.isEmpty());
