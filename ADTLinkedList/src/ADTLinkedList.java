b77acafe8e7a4ba787444341f6fc0f87384a4a5b
Pras C <prashathc@gmail.com>
2020-12-21 22:22:23 -0500

    adding comments to linked list code

--- ADTLinkedList/src/ADTLinkedList.java
+++ ADTLinkedList/src/ADTLinkedList.java
@@ -1,7 +1,7 @@
-public class ADTLinkedList<T> {
+public class ADTLinkedList<T> {         // <T> signifies generic type, can be replaced by anything
 
     private class Node {
-        T item;
+        T item;                     // item of type T  can be String, Integer, Double, etc
         Node next;
 
         public Node(T item, Node next) {
@@ -17,50 +17,19 @@ public class ADTLinkedList<T> {
     }
 
     public boolean isEmpty() {
-        return head == null;
+        return head == null;          // head == null will evaluate to true, if linked list is empty, otherwise false
     }
 
     public int size() {
         int count = 0;
 
-        Node curr = head;
-        while (curr != null) {
+        Node curr = head;           // start at beginning of lineked list
+        while (curr != null) {      // loop until tail
             curr = curr.next;
-            count++;
+            count++;                // inc count for each additional link
         }
 
         return count;
-    }
-
-    public void add(T item, int pos) {
-        Node curr = head;
-        Node prev = null;
-
-        while (pos > 1 && curr != null) {
-            prev = curr;
-            curr = curr.next;
-            pos--;
-        }
-
-        if (prev == null) {
-            head = new Node(item, curr);
-        } else {
-            prev.next = new Node(item, curr);
-        }
-    }
-
-    public void remove(int pos) {
-        if (isEmpty()) throw new RuntimeException("The list is empty");
-        if (pos < 1 || pos > size()) throw new RuntimeException("The position is invalid");
-
-        Node prev = find(pos);
-        Node curr = prev == null ? head : prev.next;
-
-        if (prev == null) {
-            head = curr.next;
-        } else {
-            prev.next = curr.next;
-        }
     }
 
     public T get(int pos) {
@@ -84,6 +53,40 @@ public class ADTLinkedList<T> {
         }
 
         return prev;
+    }
+
+    // idea is to start at head and loop through chain until we find correct position
+    // if list is empty, add at head
+    // if list is not empty, add new node after prev and before curr
+    public void add(T item, int pos) {
+        Node curr = head;
+        Node prev = null;
+
+        while (pos > 1 && curr != null) {
+            prev = curr;
+            curr = curr.next;
+            pos--;
+        }
+
+        if (prev == null) {
+            head = new Node(item, curr);        // adding to beginning of linked list
+        } else {
+            prev.next = new Node(item, curr);
+        }
+    }
+
+    public void remove(int pos) {
+        if (isEmpty()) throw new RuntimeException("The list is empty");
+        if (pos < 1 || pos > size()) throw new RuntimeException("The position is invalid");
+
+        Node prev = find(pos);
+        Node curr = prev == null ? head : prev.next;
+
+        if (prev == null) {
+            head = curr.next;
+        } else {
+            prev.next = curr.next;
+        }
     }
 
     public void removeAll() {
