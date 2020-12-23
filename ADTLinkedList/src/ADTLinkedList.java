393817513e5bf8def21e0bac97e7943036db32b4
prashathc <prashathc@gmail.com>
2020-12-23 01:25:02 -0500

    completed linked list implementation

--- ADTLinkedList/src/ADTLinkedList.java
+++ ADTLinkedList/src/ADTLinkedList.java
@@ -17,77 +17,78 @@ public class ADTLinkedList<T> {         // <T> signifies generic type, can be re
     }
 
     public boolean isEmpty() {
-        return head == null;          // head == null will evaluate to true, if linked list is empty, otherwise false
+        return head == null;
     }
+
+    public void addToFront(T item) {
+        if (isEmpty()) {
+            head = new Node(item, null);
+        } else {
+            head = new Node(item, head);
+        }
+    }
+
+    public void addToBack(T item) {
+        if (isEmpty()) {
+            head = new Node(item, null);
+        } else {
+            Node curr = head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            curr.next = new Node(item, null);
+        }
+    }
+
+    public void removeFromFront() {
+        if (isEmpty()) throw new RuntimeException("Nothing to remove");
+        head = head.next;
+    }
+
+    public void removeFromBack() {
+        if (isEmpty()) throw new RuntimeException("Nothing to remove");
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr.next != null) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        prev.next = null;
+    }
+
+    public T get(int pos) {
+        if (pos < 1) throw new RuntimeException("Position is invalid");
+
+        Node curr = head;
+        while (curr != null && pos != 1) {
+            curr = curr.next;
+            pos--;
+        }
+
+        if (curr == null) throw new RuntimeException("No item at this position");
+
+        return curr.item;
+
+    }
+
+
+
 
     public int size() {
         int count = 0;
 
-        Node curr = head;           // start at beginning of lineked list
-        while (curr != null) {      // loop until tail
+        Node curr = head;
+        while (curr != null) {
             curr = curr.next;
-            count++;                // inc count for each additional link
+            count++;
         }
 
         return count;
-    }
-
-    // returns null or returns the item at the position indicated
-    private Node find(int pos) {
-        Node curr = head;
-        Node prev = null;
-
-        while (pos > 1 && curr != null) {   // loop through linked list until pos is 1 or curr = tail
-            prev = curr;
-            curr = curr.next;
-            pos--;
-        }
-
-        return prev;
-    }
-
-    public T get(int pos) {
-        if (isEmpty()) throw new RuntimeException("The list is empty");
-        if (pos < 1 || pos > size()) throw new RuntimeException("The position is invalid");
-
-        Node prev = find(pos);
-        Node curr = prev == null ? head : prev.next;
-
-        return curr.item;
-    }
-
-    // idea is to start at head and loop through chain until we find correct position
-    // if list is empty, add at head
-    // if list is not empty, add new node after prev and before curr
-    public void add(T item, int pos) {
-        Node curr = head;
-        Node prev = null;
-
-        while (pos > 1 && curr != null) {
-            prev = curr;
-            curr = curr.next;
-            pos--;
-        }
-
-        if (prev == null) {
-            head = new Node(item, curr);        // adding to beginning of linked list
-        } else {
-            prev.next = new Node(item, curr);
-        }
-    }
-
-    public void remove(int pos) {
-        if (isEmpty()) throw new RuntimeException("The list is empty");
-        if (pos < 1 || pos > size()) throw new RuntimeException("The position is invalid");
-
-        Node prev = find(pos);
-        Node curr = prev == null ? head : prev.next;
-
-        if (prev == null) {
-            head = curr.next;
-        } else {
-            prev.next = curr.next;
-        }
     }
 
     public void removeAll() {
--- ADTLinkedList/src/TestADTLinkedList.java
+++ ADTLinkedList/src/TestADTLinkedList.java
@@ -2,17 +2,22 @@ public class TestADTLinkedList {
     public static void main(String[] args) {
         ADTLinkedList linkedList = new ADTLinkedList<String>();
 
-        linkedList.add("Kobe Bryant", 1);
-        linkedList.add("Wilt Chamberlain", 2);
-        linkedList.add("Jrue Holiday", 3);
+        linkedList.addToFront("Kobe Bryant");
+        linkedList.addToFront("Wilt Chamberlain");
+        linkedList.addToFront("Jrue Holiday");
+        linkedList.addToBack("Kyle Lowry");
 
         System.out.println("The second item is: " + linkedList.get(2));
         System.out.println("The third item is: " + linkedList.get(3));
+        System.out.println("The last item is: " + linkedList.get(4));
+
+        System.out.println();
         System.out.println("The size is: " + linkedList.size());
 
-        linkedList.remove(2);
-        System.out.println("The second item is: " + linkedList.get(2));
+        linkedList.removeFromBack();
+        System.out.println("The size is: " + linkedList.size());
 
+        System.out.println();
         System.out.println("Linked list is empty: " + linkedList.isEmpty());
         linkedList.removeAll();
         System.out.println("Linked list is empty: " + linkedList.isEmpty());
